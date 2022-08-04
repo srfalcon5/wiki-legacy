90202a37c6a7e0638e1fa0e1b73fca145becc229
Matthew Ronchetto <hello@doamatto.xyz>
2022-08-04 07:35:03 -0700

    chore: start of search script

---
+++ static/search.js
@@ -0,0 +1,14 @@
+function debounce(func, wait) {
+  var timeout;
+
+  return function () {
+    var context = this;
+    var args = arguments;
+    clearTimeout(timeout);
+
+    timeout = setTimeout(function () {
+      timeout = null;
+      func.apply(context, args);
+    }, wait);
+  };
+}
